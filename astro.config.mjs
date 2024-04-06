f5093c88f868935bb211264a285460236a12bf12
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2024-04-06 13:58:56 +1100

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,23 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
-
 import db from "@astrojs/db";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
   integrations: [db()],
   vite: {
     optimizeDeps: {
-      exclude: ["oslo"],
-    },
+      exclude: ["oslo"]
+    }
   },
   server: {
     headers: {
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers":
-        "Origin, X-Requested-With, Content-Type, Accept",
-    },
-  },
+      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+    }
+  }
 });
