fa7ad5fc4cf3ccecaf2453368b7120b7a6fc5662
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2024-04-12 09:11:01 +0200

    make easier to add new bookmarks

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,13 +11,15 @@ export default defineConfig({
   integrations: [db()],
   vite: {
     optimizeDeps: {
-      exclude: ["oslo"]
-    }
+      exclude: ["oslo"],
+    },
   },
   server: {
     headers: {
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
-    }
-  }
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept",
+    },
+  },
 });
+
